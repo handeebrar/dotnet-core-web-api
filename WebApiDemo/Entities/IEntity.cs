87cdb54dc81465f9e04dd1074142c696762aa9af
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-09 10:50:16 +0300

    entity folder and product entity were created

---
+++ WebApiDemo/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Entities
+{
+    //bu interface'i implement eden sınıflar benim için db nesnesidir anlamına gelir
+    interface IEntity
+    {
+    }
+}
