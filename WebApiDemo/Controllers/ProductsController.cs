23fa0bf34f7a8770889e847a63a006bc5f874a89
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-09 14:24:39 +0300

    get response was added

--- WebApiDemo/Controllers/ProductsController.cs
+++ WebApiDemo/Controllers/ProductsController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.DataAccess;
 
 namespace WebApiDemo.Controllers
 {
     [Route("api/products")]
     public class ProductsController : Controller
     {
-        [HttpGet("")]
-        public string Get()
+        IProductDal _productDal;
+        public ProductsController(IProductDal productDal) //IProductDal'ın ne olduğunu bilmediği için Startup.cs'teki ConfigureServices()'e gidecek
         {
-            return "Product1";
+            _productDal = productDal;
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var products = _productDal.GetList();
+            return Ok(products);
         }
     }
 }
