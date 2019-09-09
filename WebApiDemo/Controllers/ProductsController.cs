dcda67690be1333ebdee86d354fcadc3f50b9891
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-09 10:14:29 +0300

    Add project files.

---
+++ WebApiDemo/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        public string Get()
+        {
+            return "Product1";
+        }
+    }
+}
