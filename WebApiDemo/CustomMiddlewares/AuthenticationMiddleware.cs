8b18817995f81ff8491a8560d44b07a8a5ecb6c3
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-10 10:07:17 +0300

    basic authentication middleware

--- WebApiDemo/CustomMiddlewares/AuthenticationMiddleware.cs
+++ WebApiDemo/CustomMiddlewares/AuthenticationMiddleware.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApiDemo.CustomMiddlewares
@@ -18,7 +20,40 @@ namespace WebApiDemo.CustomMiddlewares
 
         public async Task Invoke(HttpContext context)
         {
-            var authHeader = context.Request.Headers["Authorization"];
+            string authHeader = context.Request.Headers["Authorization"]; //basic hande:12345
+
+            if (authHeader != null && authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase)) //autheader var ve basicle başlıyorsa
+            {
+                var token = authHeader.Substring(6).Trim(); //hande:12345
+                var credentialString = "";
+
+                try
+                {
+                    credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token)); //base64'ten encode 
+                }
+                catch (Exception)
+                {
+
+                    context.Response.StatusCode = 500;
+                }
+
+                var credentials = credentialString.Split(':');
+
+                if (credentials[0] == "hande" && credentials[1] == "12345")
+                {
+                    var claims = new[]
+                    {
+                        new Claim("name", credentials[0]),
+                        new Claim(ClaimTypes.Role, "Admin"),
+                    };
+                    var identity = new ClaimsIdentity(claims);
+                    context.User = new ClaimsPrincipal(identity);
+                }
+                else
+                {
+                    context.Response.StatusCode = 401; //unauthrorized
+                }
+            }
 
             await _next(context);
         }
